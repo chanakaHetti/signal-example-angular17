1c219bdd54f5b7ac24d38b6616a4dfdacabd253c
chanakaHetti <cdileep40@gmail.com>
2024-11-24 12:43:46 +0000

    Creating the table filter using the computed signal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component, signal, effect, computed } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
@@ -15,10 +16,23 @@ export class AppComponent {
   theme = signal('light');
   label = this.theme();
 
-  // Calculated signal
+  // Computed signal
   price = 19;
   quantity = signal(10);
   totalPrice = computed(() => this.price * this.quantity());
+
+  // Signal usecase with Computed signal
+  products = signal([
+    { id: 1, name: 'Milk', price: 1.45 },
+    { id: 2, name: 'Bread', price: 2.9 },
+    { id: 3, name: 'Chicken', price: 3.25 },
+  ]);
+  filterName = signal('');
+  filteredProducts = computed(() => {
+    return this.products().filter((product) =>
+      product.name.toLowerCase().includes(this.filterName().toLowerCase())
+    );
+  });
 
   constructor() {
     console.log('constructor run');
@@ -48,5 +62,10 @@ export class AppComponent {
 
   changeQuantity(event: Event) {
     this.quantity.set((event.target as HTMLInputElement).valueAsNumber);
+  }
+
+  changeFilter(event: Event) {
+    let newFilterName = (event.target as HTMLInputElement).value;
+    this.filterName.set(newFilterName);
   }
 }
