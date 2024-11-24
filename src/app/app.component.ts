a918416db79f5e8073a63e617ced139d9a5e1698
chanakaHetti <cdileep40@gmail.com>
2024-11-24 12:21:45 +0000

    Signal usage with dom element change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, effect, computed } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -11,14 +11,34 @@ import { RouterOutlet } from '@angular/router';
 export class AppComponent {
   title = 'signal-example-angular17';
 
+  // Signal
   theme = signal('light');
+  label = this.theme();
+
+  // Calculated signal
+  price = 19;
+  quantity = signal(10);
+  totalPrice = computed(() => this.price * this.quantity());
+
+  constructor() {
+    console.log('constructor run');
+    effect(() => {
+      console.log('effect run');
+      this.label = this.theme();
+      document.body.className = this.theme();
+    });
+  }
 
   ngOnInit() {
+    console.log('ngOnInit run');
     // this.theme.set('dark');
     // this.theme.set(this.theme === 'light' ? 'dark' : 'light);
+    // document.body.className = this.theme();
+  }
+
+  toggleDarkMode() {
     this.theme.update((currentValue) =>
       currentValue === 'light' ? 'dark' : 'light'
     );
-    document.body.className = this.theme();
   }
 }
